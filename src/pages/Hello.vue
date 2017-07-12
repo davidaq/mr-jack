1e66cc3026ff50a24ab627f76968806ed1d2f100
anqin <153019747@qq.com>
2017-07-13 00:06:23 +0800

    img

--- src/pages/Hello.vue
+++ src/pages/Hello.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="hello">
-        <h1 class="gray--text">Mr. Jack</h1>
-        <h4 class="gray--text">开膛手杰克</h4>
+        <h1 class="logo"></h1>
 
         <ul>
             <li><router-link to="/game?vs=tutor">教学关</router-link></li>
@@ -27,12 +26,20 @@
 <style lang="stylus" scoped>
 .hello
     height 100%
-    background url(../assets/hello-bg.jpg) no-repeat
+    background url(../assets/hello-bg.jpg) no-repeat center center
     background-size cover
     &::before
         content ""
         display block
         height 1px
+
+.logo
+    background url(../assets/jack-logo.png) no-repeat center center
+    background-size cover
+    height 250px
+    width 250px
+    margin: 0 auto
+    filter invert(1)
 
 h1
     margin-top 100px
@@ -59,11 +66,11 @@ a
     width 150px
     height 45px
     font-weight bold
-    background #333
+    background rgba(0, 0, 0, 0.5)
     text-decoration none
     color #fff
-    transition: background 0.3s
-    &:hover
+    transition background 0.3s
+    &:hover, &:active
         background #444
 
 </style>
